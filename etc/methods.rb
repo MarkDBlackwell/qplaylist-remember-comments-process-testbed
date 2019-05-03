d40f53d77472e6b28b4ef23f98dc704848d09bfa
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-05-03 16:37:24 -0400

    Strip initial comments

--- etc/methods.rb
+++ etc/methods.rb
@@ -1,13 +1,3 @@
-# coding: utf-8
-
-=begin
-Copyright (C) 2018 Mark D. Blackwell.
-   All rights reserved.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-=end
-
 require 'English'
 
 module ::QplaylistRememberCommentsProcess
